84080a8af5681a9e0201e0b93d1d442649e7f94d
Lee, Dakyum <dlee@lcs-mba3088.lcs.on.ca>
2017-10-12 14:02:30 -0400

    Finish Challenge

--- IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -34,13 +34,32 @@ let canvas = Canvas(width: 300, height: 300)
 canvas.defaultLineWidth = 1
 
 // Draw the "rungs" of a ladder
-for x in stride (from: 25, through: 275, by: 50){
-    for y in stride (from: 275, through: 25, by: -50){
-        canvas.drawLine(fromX: x-10, fromY: y-10, toX: x+10, toY: y+10)
-        canvas.drawLine(fromX: x+10, fromY: y-10, toX: x-10, toY: y+10)
-    }
-}
+for y in stride(from: 10, through: 400, by: 50) {
+    
+    y
 
+    
+    // Draw line
+    canvas.drawLine(fromX: 0, fromY: 0, toX: 300, toY: 0)
+    canvas.drawLine(fromX: 20, fromY: 0, toX: 300, toY:20)
+    canvas.drawLine(fromX: 40, fromY: 0, toX: 300, toY:40)
+    canvas.drawLine(fromX: 60, fromY: 0, toX: 300, toY:60)
+    canvas.drawLine(fromX: 80, fromY: 0, toX: 300, toY:80)
+    canvas.drawLine(fromX: 100, fromY: 0, toX: 300, toY:100)
+    canvas.drawLine(fromX: 120, fromY: 0, toX: 300, toY:120)
+    canvas.drawLine(fromX: 140, fromY: 0, toX: 300, toY:140)
+    canvas.drawLine(fromX: 160, fromY: 0, toX: 300, toY:160)
+    canvas.drawLine(fromX: 180, fromY: 0, toX: 300, toY:180)
+    canvas.drawLine(fromX: 200, fromY: 0, toX: 300, toY:200)
+    canvas.drawLine(fromX: 220, fromY: 0, toX: 300, toY:220)
+    canvas.drawLine(fromX: 240, fromY: 0, toX: 300, toY:240)
+    canvas.drawLine(fromX: 260, fromY: 0, toX: 300, toY:260)
+    canvas.drawLine(fromX: 280, fromY: 0, toX: 300, toY:280)
+    canvas.drawLine(fromX: 300, fromY: 0, toX: 300, toY:300)
+
+    canvas
+    
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
