7ff719c03db53de425b961ac9e4f652003b96f3b
Lee, Dakyum <dlee@lcs-mba3088.lcs.on.ca>
2017-10-16 09:23:51 -0400

    Exercise

--- IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -34,30 +34,27 @@ let canvas = Canvas(width: 300, height: 300)
 canvas.defaultLineWidth = 1
 
 // Draw the "rungs" of a ladder
-for y in stride(from: 10, through: 400, by: 50) {
+for x in stride (from: 25, through: 275, by: 50){
     
-    y
+    print("X VALUE IS: \(x)")
+   
+    for y in stride (from: 275, through: 25, by: -50) {
+        
+        print("y value is: \(y)")
 
-    
-    // Draw line
-    canvas.drawLine(fromX: 0, fromY: 0, toX: 300, toY: 0)
-    canvas.drawLine(fromX: 20, fromY: 0, toX: 300, toY:20)
-    canvas.drawLine(fromX: 40, fromY: 0, toX: 300, toY:40)
-    canvas.drawLine(fromX: 60, fromY: 0, toX: 300, toY:60)
-    canvas.drawLine(fromX: 80, fromY: 0, toX: 300, toY:80)
-    canvas.drawLine(fromX: 100, fromY: 0, toX: 300, toY:100)
-    canvas.drawLine(fromX: 120, fromY: 0, toX: 300, toY:120)
-    canvas.drawLine(fromX: 140, fromY: 0, toX: 300, toY:140)
-    canvas.drawLine(fromX: 160, fromY: 0, toX: 300, toY:160)
-    canvas.drawLine(fromX: 180, fromY: 0, toX: 300, toY:180)
-    canvas.drawLine(fromX: 200, fromY: 0, toX: 300, toY:200)
-    canvas.drawLine(fromX: 220, fromY: 0, toX: 300, toY:220)
-    canvas.drawLine(fromX: 240, fromY: 0, toX: 300, toY:240)
-    canvas.drawLine(fromX: 260, fromY: 0, toX: 300, toY:260)
-    canvas.drawLine(fromX: 280, fromY: 0, toX: 300, toY:280)
-    canvas.drawLine(fromX: 300, fromY: 0, toX: 300, toY:300)
-
-    canvas
+        let i = random(from: 1, toButNotIncluding: 3)
+        
+        if i == 1 {
+            //draw increasing line eg. /
+            canvas.drawLine(fromX: x-25, fromY: y-25, toX: x+25, toY: y+25)
+        } else {
+            //draw decreasing line eg. \
+            canvas.drawLine(fromX: x+25, fromY: y-25, toX: x-25, toY: y+25)
+        }
+        
+        
+       
+    }
     
 }
 /*:
